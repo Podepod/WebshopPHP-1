5b15e540e21c94f39e5fad781a35a4425760c487
Sander <sander.vandeven86@gmail.com>
2020-12-20 14:29:37 -0800

    test

--- sign_in_page.php
+++ sign_in_page.php
@@ -42,9 +42,9 @@
       <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
       <button class="btn btn-outline-light btn-lg" type="submit" id="Signin" name="Signin">Log in</button>
     </form>
-    <div class="d-flex justify-content-center"> 
+    <!--<div class="d-flex justify-content-center"> 
       <a href="sign_up_page.php" class="btn btn-primary stretched-link">Registreer je hier</a>
-    </div>
+    </div> -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
   </body>
