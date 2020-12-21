61e3733cdc5f67b26d1a90e2848f5d4009fb609f
Sander <sander.vandeven86@gmail.com>
2020-12-21 05:20:24 -0800

    test lay-out sign in

--- sign_in_page.php
+++ sign_in_page.php
@@ -33,6 +33,8 @@
     <link href="styles/signin.css" rel="stylesheet">
   </head>
   <body class="text-center">
+  <div class="container">
+    <div class="row">
     <form class="form-signin" action="includes/sign_in.php" method="POST">
       <img class="mb-4" src="images/GESS_logo_light.png" alt="" width="144" height="108">
       <h1 class="h3 mb-3 font-weight-normal text-light">Sign in here</h1>
@@ -42,9 +44,13 @@
       <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
       <button class="btn btn-outline-light btn-lg" type="submit" id="signin" name="signin">Log in</button>
     </form>
-    <!--<div class="d-flex justify-content-center"> 
-      <a href="sign_up_page.php" class="btn btn-primary stretched-link">Registreer je hier</a>
-    </div> -->
+    </div>
+    <div class="row">
+      <div class="d-flex justify-content-center"> 
+        <a href="sign_up_page.php" class="btn btn-primary stretched-link">Registreer je hier</a>
+      </div>
+    </div>
+  </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
   </body>
