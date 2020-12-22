eb0690e992003c95985bceeceaa716720ed0dd0e
Sander <sander.vandeven86@gmail.com>
2020-12-22 04:52:33 -0800

    test without image_path

--- products.php
+++ products.php
@@ -71,14 +71,14 @@
       echo('<div class="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">');
       echo('<div class="my-3 py-3">');
       echo('<h2 class="display-5">' . $row['name'] . '</h2>');
-      if($row['image_path'])
-      {
-        echo('<img src="' . $row['image_path'] . '" alt="Product image">');
-      }
-      else
-      {
-        echo('<p>No image</p>');
-      }
+      #if($row['image_path'])
+      #{
+      #  echo('<img src="' . $row['image_path'] . '" alt="Product image">');
+      #}
+      #else
+      #{
+      #  echo('<p>No image</p>');
+      #}
       echo('</div>');
       echo('<div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>');
       echo('</div>');
