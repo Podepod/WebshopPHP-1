9e0d320cbcd088dbd2e17bc4f4299fd06c4946b0
Sander <sander.vandeven86@gmail.com>
2020-12-23 07:53:44 -0800

    Test

--- includes/place_order.php
+++ includes/place_order.php
@@ -1,7 +1,7 @@
 <?php
     if(isset($_POST['submit-order']))
     {
-        require 'config.php';
+        require "config.php";
         if(isset($_SESSION['CustomerID']))
         {
             $CustomerID = $_SESSION['CustomerID'];
