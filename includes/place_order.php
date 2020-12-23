c94ac24014c668a8c7f432e2afbe7656e761f07d
Sander <sander.vandeven86@gmail.com>
2020-12-23 07:22:29 -0800

    testing

--- includes/place_order.php
+++ includes/place_order.php
@@ -17,23 +17,23 @@
                 $sql = "SELECT OrderID FROM Orders WHERE CustomerID=" . $_SESSION['CustomerID'] . "ORDER BY OrderID DESC LIMIT 1;";
                 $stmt = mysqli_stmt_init($conn);
                 if(!mysqli_stmt_prepare($stmt, $sql))
-                    {
-                        header("Location: ../register.php?error=sqlerror2");
-                    }
-                    else
-                    {
-                        mysqli_stmt_execute($stmt);
-                        $result = mysqli_stmt_get_result($stmt);
-                        $row = mysqli_fetch_assoc($result);
-                        $OrderID = $row['OrderID'];
-                    }
+                {
+                    header("Location: ../checkout.php?error=sqlerror2");
+                }
+                else
+                {
+                    mysqli_stmt_execute($stmt);
+                    $result = mysqli_stmt_get_result($stmt);
+                    $row = mysqli_fetch_assoc($result);
+                    $OrderID = $row['OrderID'];
+                }
                 foreach($_SESSION["shopping-cart"] as $item)
                 {
                     $sql = "INSERT INTO Order_details (OrderID, ProductID, amount) VALUES (" . $OrderID . "," . $item['ProductID'] . "," . $item['Quantity'] . ");";
                     $stmt = mysqli_stmt_init($conn);
                     if(!mysqli_stmt_prepare($stmt, $sql))
                     {
-                        header("Location: ../register.php?error=sqlerror3");
+                        header("Location: ../checkout.php?error=sqlerror3");
                     }
                     else
                     {
@@ -47,5 +47,9 @@
         {
             header("Location= ../checkout.php?error=NoUserSignedIn");
         }
+    }
+    else
+    {
+        header("Location= ../checkout.php?error=NoButtonPressed");
     }
 ?>
